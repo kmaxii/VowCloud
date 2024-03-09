98dd828f1229b57ba6f67bc93980309dae5c8e54
kmaxii <esst936@gmail.com>
2024-03-09 22:23:19 +0100

    Upgraded to version 1.4

--- src/main/java/me/kmaxi/vowcloud/VowCloud.java
+++ src/main/java/me/kmaxi/vowcloud/VowCloud.java
@@ -21,7 +21,7 @@ public class VowCloud implements ModInitializer {
     public VowConfig config;
 
     public static String MODID = "vowcloud";
-    public static final String VERSION = "1.3";
+    public static final String VERSION = "1.4";
 
 
     public static VowCloud getInstance(){
