43afec463f9f88f8f4b9559df5a296ab0d1c591b
kmaxii <esst936@gmail.com>
2024-07-07 23:07:05 +0200

    Fixed detection of some lines

--- src/main/java/me/kmaxi/vowcloud/VowCloud.java
+++ src/main/java/me/kmaxi/vowcloud/VowCloud.java
@@ -24,7 +24,7 @@ public class VowCloud implements ModInitializer {
     public static VowConfig CONFIG;
 
     public static String MODID = "vowcloud";
-    public static final String VERSION = "1.5";
+    public static final String VERSION = "1.4";
 
 
     public static VowCloud getInstance() {
